a337a9cc779a368a48d6318c6ed4f8534b16ddc3
TalexCK <84849086+TalexCK@users.noreply.github.com>
2023-06-18 16:05:23 +0800

    Add files via upload

---
+++ src/main/java/com/pj/chess/LogWindow.java
@@ -0,0 +1,50 @@
+package com.pj.chess;
+
+import javax.swing.*;
+import javax.swing.text.DefaultCaret;
+
+public class LogWindow {
+    public LogWindow() {
+        try {
+            jtextArea.append("AI日志开启\n");
+        }
+        catch (Exception ev) {
+
+        }
+    }
+    public static void addlog(String info){
+        try {
+            jtextArea.append("[INFO] "+info+"\n");
+        }
+        catch (Exception ev) {
+
+        }
+    }
+    public static void addlogerror(String error,Integer level){
+        try {
+            jtextArea.append("[ERROR] "+level+"级 "+error+"\n");
+            if(level > 0){
+                JOptionPane.showMessageDialog(null, "Warning","程序出现"+level+"级错误",JOptionPane.ERROR_MESSAGE);
+            }
+        }
+        catch (Exception ev) {
+
+        }
+    }
+    public static JTextArea jtextArea= new JTextArea(1000,300);
+    public static void logwindow() {
+        JFrame jframe=new JFrame("日志记录");
+        jframe.setBounds(100,100,600,1200);
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jtextArea.setEditable(false);
+        DefaultCaret caret=(DefaultCaret)jtextArea.getCaret();
+        caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
+        jtextArea.setLineWrap(true);
+        jtextArea.setWrapStyleWord(true);
+        jframe.add(jtextArea);
+        jframe.setVisible(true);
+
+        new LogWindow();
+    }
+
+}
---
+++ src/main/java/com/pj/chess/VersionFile.java
@@ -0,0 +1,20 @@
+package com.pj.chess;
+
+public class VersionFile {
+    public static String version = "0.0.2";
+    public static String Updatedate = "2023.05.14";
+    public static String Updatemessage = "1.增加棋谱功能";
+    public static String BugFixmessage = "1.修复配置文件读取错误";
+    public static String publishpage = "https://talexck.space/2023/05/18/ChineseChess.html";
+    public static String Githublink = "https://github.com/TalexCK/ChineseChess";
+    public static String LatestReleases = "https://github.com/TalexCK/ChineseChess/releases/latest";
+    public static String LatestTag = "https://github.com/TalexCK/ChineseChess/releases/tag";
+    public static String AboutMessage = "";
+    public static boolean AlphaVersion = true;
+    public static String alphaversion = "0.0.2.4";
+    public static String alphaUpdateDate = "2023.06.18";
+    public static String alphaUpdatemessage = "1.修改配置文件系统";
+    public static String alphaBugFixmessage = "1.JAR包内容精简";
+    public static String versionagreed = "";
+
+}
