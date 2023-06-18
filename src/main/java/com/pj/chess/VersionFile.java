b1b3995a235d82d42a7ad15710bf8ca22d5fe97f
TalexCK <84849086+TalexCK@users.noreply.github.com>
2023-06-18 22:20:52 +0800

    0.1-beta

--- src/main/java/com/pj/chess/VersionFile.java
+++ src/main/java/com/pj/chess/VersionFile.java
@@ -11,10 +11,10 @@ public class VersionFile {
     public static String LatestTag = "https://github.com/TalexCK/ChineseChess/releases/tag";
     public static String AboutMessage = "";
     public static boolean AlphaVersion = true;
-    public static String alphaversion = "0.0.2.5";
+    public static String alphaversion = "0.1-beta";
     public static String alphaUpdateDate = "2023.06.18";
-    public static String alphaUpdatemessage = "0.1-beta版本更新准备工作";
-    public static String alphaBugFixmessage = "";
+    public static String alphaUpdatemessage = "1.配置文件更新\n2.添加AI对战AI的功能\n3.优化版本信息";
+    public static String alphaBugFixmessage = "1.修复棋谱记录不删除错误\n2.修复棋谱可以无限进行 下一步 的错误\n"+"3.修复棋谱可以无限进行'上一步'的错误\n4.修复棋谱无法进行'下一步'的错误";
     public static String versionagreed = "";
 
 }
